e0fa2c0c7deace5964ccfeb9bf821143e62257d3
Kumail7 <kumailrizvi70@gmail.com>
2024-04-21 23:06:31 +0530

    Adding changes in pipeline

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,9 +1,9 @@
 terraform {
   backend "s3" {
     encrypt = true    
-    bucket = "terrform-state-aws"
+    bucket = "tf-states-aws"
     dynamodb_table = "terraform-state-lock-dynamo"
-    key    = "terraform.tfstate"
+    key    = "tf.tfstate"
     region = "ap-south-1"
   }
 }
