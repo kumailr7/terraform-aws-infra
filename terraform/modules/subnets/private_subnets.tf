7c67bfb5396be3d8f240f3f7140901b890cc8bf5
kumailr7 <kumailrizvi70@gmail.com>
2024-04-22 22:22:33 +0530

    Added Changes in alb , subnets output.tf and modules

--- terraform/modules/subnets/private_subnets.tf
+++ terraform/modules/subnets/private_subnets.tf
@@ -30,7 +30,7 @@ resource "aws_route_table" "private_route_table" {
 
 # Associate public route table with public subnet
 resource "aws_route_table_association" "subnet_private_route_table_association" {
-   count             = length(aws_subnet.private_subnet)
+  count             = length(aws_subnet.private_subnet)
   subnet_id      = aws_subnet.private_subnet[count.index].id
   route_table_id = aws_route_table.private_route_table.id
 }
--- terraform/modules/subnets/public_subnets.tf
+++ terraform/modules/subnets/public_subnets.tf
@@ -31,7 +31,7 @@ resource "aws_route_table" "public_route_table" {
 # Associate public route table with public subnet
 resource "aws_route_table_association" "subnet_route_table_association" {
   count             = length(aws_subnet.public_subnet)
-  subnet_id      = aws_subnet.public_subnet.id
+  subnet_id      = aws_subnet.public_subnet[count.index].id
   route_table_id = aws_route_table.public_route_table.id
 }
 
