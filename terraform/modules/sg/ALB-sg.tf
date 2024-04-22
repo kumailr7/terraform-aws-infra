3c0f1a982b817fa578e30351cd186d887eb61401
kumailr7 <kumailrizvi70@gmail.com>
2024-04-22 23:56:52 +0530

    Added Changes in alb , subnets output.tf and modules

--- terraform/modules/sg/ALB-sg.tf
+++ terraform/modules/sg/ALB-sg.tf
@@ -8,7 +8,7 @@ resource "aws_security_group" "alb_sg" {
       from_port   = ingress.value.from_port
       to_port     = ingress.value.to_port
       protocol    = ingress.value.protocol
-      cidr_blocks = ingress.value.cidr_block
+      cidr_blocks = ingress.value.cidr_blocks
     }
   }
 
